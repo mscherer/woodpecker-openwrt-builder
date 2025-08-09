f30c00ab9f6ccf5c249f4f67338646b033415446
Michael Scherer <misc@redhat.com>
2025-08-09 21:03:37 +0200

    Simplify the script
    since there is no need for sha256sum, we can remove it. Then this mean
    there is no need for IMAGE_NAME, so we can remove it. And so we can
    remove FILESYSTEM. And since FILES should be files by default (as that's
    default on
    https://openwrt.org/docs/guide-user/additional-software/imagebuilder ),
    we can change it as well.

--- build.sh
+++ build.sh
@@ -7,8 +7,7 @@ CONFIG_FILE=builder.conf
 source builder.conf
 
 PROFILE=${PROFILE:-generic}
-FILES=${FILES:-config}
-FILESYSTEM=${FILESYSTEM:-squashfs}
+FILES=${FILES:-files}
 
 [[ -v RELEASE ]] || (echo "RELEASE is not set in $CONFIG_FILE" ; exit 1)
 [[ -v TARGET ]] || (echo "TARGET is not set in $CONFIG_FILE" ; exit 1)
@@ -16,7 +15,6 @@ FILESYSTEM=${FILESYSTEM:-squashfs}
 
 CURRENT_DIR=$(pwd)
 EXT=zst
-IMAGE_NAME=openwrt-${RELEASE}-${EXTRA_IMAGE_NAME}-${TARGET}-${BOARD}-${PROFILE}-${FILESYSTEM}-sysupgrade.bin
 
 cd /tmp
 wget https://downloads.openwrt.org/releases/${RELEASE}/targets/${TARGET}/${BOARD}/openwrt-imagebuilder-${RELEASE}-${TARGET}-${BOARD}.$(uname -s)-$(uname -m).tar.${EXT}
@@ -30,5 +28,4 @@ mv ./bin/targets/${TARGET}/${BOARD}/ ${CURRENT_DIR}/build
 cd ${CURRENT_DIR}/build
 
 ls -l
-sha256sum ${IMAGE_NAME}
 # TODO embeded the current git short id in the name, as well as the date, see https://woodpecker-ci.org/docs/usage/environment
