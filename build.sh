d67011cac6e7f5dbce02fb5bde20b7fd710d9908
Michael Scherer <misc@redhat.com>
2025-08-09 10:58:30 +0200

    Remove some TODOs
    The copying part is better suited for a external plugin
    And the verification of the config is done
    
    Also fix error for variable detection, and copy all data

--- build.sh
+++ build.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 set -eux
 
-# TODO verify the file exist and show a proper error
+CONFIG_FILE=builder.conf
+[[ -f $CONFIG_FILE ]] ||  (echo "configuration $CONFIG_FILE not found" ; exit 1)
+
 source builder.conf
 
 PROFILE=${PROFILE:-generic}
 FILES=${FILES:-config}
 
-[[ -v RELEASE ]] || "RELEASE is not set"
-[[ -v TARGET ]] || "TARGET is not set"
-[[ -v BOARD ]] || "BOARD is not set"
-
-#TODO add potential templating with envsubst
+[[ -v RELEASE ]] || (echo "RELEASE is not set in $CONFIG_FILE" ; exit 1)
+[[ -v TARGET ]] || (echo "TARGET is not set in $CONFIG_FILE" ; exit 1)
+[[ -v BOARD ]] || (echo "BOARD is not set in $CONFIG_FILE" ; exit 1)
 
 CURRENT_DIR=$(pwd)
 EXT=zst
 IMAGE_NAME=openwrt-${RELEASE}-${EXTRA_IMAGE_NAME}-${TARGET}-${BOARD}-${PROFILE}-squashfs-sysupgrade.bin
+
 cd /tmp
 wget https://downloads.openwrt.org/releases/${RELEASE}/targets/${TARGET}/${BOARD}/openwrt-imagebuilder-${RELEASE}-${TARGET}-${BOARD}.$(uname -s)-$(uname -m).tar.${EXT}
 tar -xf openwrt-imagebuilder-*
@@ -24,13 +25,9 @@ cd openwrt-imagebuilder-*
 
 make image EXTRA_IMAGE_NAME="${EXTRA_IMAGE_NAME}" PROFILE="${PROFILE}" DISABLED_SERVICES="${DISABLED_SERVICES}" PACKAGES="${PACKAGES}" FILES="${FILES}"
 
-mv ./bin/targets/${TARGET}/${BOARD}/${IMAGE_NAME} ${CURRENT_DIR}/
-cd ${CURRENT_DIR}
+mv ./bin/targets/${TARGET}/${BOARD}/ ${CURRENT_DIR}/build
+cd ${CURRENT_DIR}/build
 
 sha256sum ${IMAGE_NAME}
 ls -l ${IMAGE_NAME}
-# TODO copy to the webserver that host the image
-# TODO do some link to latest ?
-# TODO copy to the router
-# TODO run sysupgrade -v on the router
 # TODO embeded the current git short id in the name, as well as the date, see https://woodpecker-ci.org/docs/usage/environment
