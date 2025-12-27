964cd2260f67da7c4c9abc02dc20521a54eb6303
Michael Scherer <misc@redhat.com>
2025-12-27 14:50:55 +0100

    Fix files and customise.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,6 @@ set -eux
 CONFIG_FILE=builder.conf
 [[ -f $CONFIG_FILE ]] || (echo "configuration $CONFIG_FILE not found" ; exit 1)
 
-CUSTOMISE_FILE=customise.sh
-
 [[ -v PLUGIN_SSH_KEY ]] || (echo "no ssh key found, please use ssh_key setting (and a secret)" ; exit 1)
 [[ -v PLUGIN_SERVER ]] || (echo "no destination server set, please use server setting" ; exit 1)
 [[ -v PLUGIN_TARGET ]] || (echo "no target directory set, please use target setting" ; exit 1)
@@ -19,8 +17,9 @@ FILES=${FILES:-files}
 [[ -v TARGET ]] || (echo "TARGET is not set in $CONFIG_FILE" ; exit 1)
 [[ -v BOARD ]] || (echo "BOARD is not set in $CONFIG_FILE" ; exit 1)
 
-CURRENT_DIR=$(pwd)
+SOURCE_DIR=$(pwd)
 EXT=zst
+CUSTOMISE_FILE=customise.sh
 
 cd /tmp
 wget https://downloads.openwrt.org/releases/${RELEASE}/targets/${TARGET}/${BOARD}/openwrt-imagebuilder-${RELEASE}-${TARGET}-${BOARD}.$(uname -s)-$(uname -m).tar.${EXT}
@@ -28,13 +27,14 @@ tar -xf openwrt-imagebuilder-*
 rm -f openwrt-imagebuilder-*.tar.${EXT}
 cd openwrt-imagebuilder-*
 
-[[ -f ${CUSTOMISE_FILE} ]] && . ${CUSTOMISE_FILE}
+[[ -f ${SOURCE_DIR}/${CUSTOMISE_FILE} ]] && cd $SOURCE_DIR && . ${CUSTOMISE_FILE} && cd -
 
-make image EXTRA_IMAGE_NAME="${EXTRA_IMAGE_NAME}" PROFILE="${PROFILE}" DISABLED_SERVICES="${DISABLED_SERVICES}" PACKAGES="${PACKAGES}" FILES="${FILES}"
+make image EXTRA_IMAGE_NAME="${EXTRA_IMAGE_NAME}" PROFILE="${PROFILE}" DISABLED_SERVICES="${DISABLED_SERVICES}" PACKAGES="${PACKAGES}" FILES="${SOURCE_DIR}/${FILES}"
 
-mkdir -p ${CURRENT_DIR}/build/
-mv ./bin/targets/${TARGET}/${BOARD}/ ${CURRENT_DIR}/build/${CI_REPO_NAME}
-cd ${CURRENT_DIR}/build
+BUILD_DIR="${SOURCE_DIR}/build/"
+mkdir -p ${BUILD_DIR}
+mv ./bin/targets/${TARGET}/${BOARD}/ ${BUILD_DIR}/${CI_REPO_NAME}
+cd ${BUILD_DIR}
 ls -lR
 
 # TODO embed the current git short id in the name, as well as the date, see https://woodpecker-ci.org/docs/usage/environment
